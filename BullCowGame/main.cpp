e237bd586a25679534a1f66b5a21196bb90195d1
kmne68 <kmne68@gmail.com>
2018-04-26 00:35:33 -0500

    Add  initial functions

--- BullCowGame/main.cpp
+++ BullCowGame/main.cpp
@@ -3,31 +3,42 @@
 
 using namespace std;
 
+void printIntro();
+string getAndPrintGuess();
+
+// Entry point for app
 int main() {
+	
+	printIntro();
+	getAndPrintGuess();
 
-	constexpr int WORD_LENGTH = 5;
+	return 0;
+}
 
-	string guess = "";
-
+void printIntro() {
 	// introduce the game
+	constexpr int WORD_LENGTH = 5;
+	cout << WORD_LENGTH;
 	cout << "Welcome to Bulls and Cows\n";
 	cout << "Can you guess the ";
-	cout << WORD_LENGTH;
 	cout << "-letter I'm thinking of?\n";
+	return;
+}
 
-	// get a guess from the user
+
+// get a guess from the user
+string getAndPrintGuess() {
+	string guess = "";
 	cout << "What is your guess?\n";
 
 	std::getline(std::cin, guess);
 	// repeat the guess to the player
 	cout << "You guessed " << guess << "\n";
-
-
 	cout << "What is your guess?\n";
 
 	std::getline(std::cin, guess);
 
 	cout << "You guessed " << guess << "\n";
 
-	return 0;
+	return guess;
 }
